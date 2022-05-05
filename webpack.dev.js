90f0c8ef56ff840e4e826bc4b46e7e54b48a4f8c
Adolfo Herrera <valherdolf@gmail.com>
2022-05-04 21:47:26 -0400

    Webpack section completed

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,13 +3,13 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: 'development',
-  devServer: { // make webpack reload the web browser every save
-    hot: false, // optional, but you must not set both hot and liveReload to true
-    liveReload: true,
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: 'development',
+    devServer: { // make webpack reload the web browser every save
+        hot: false, // optional, but you must not set both hot and liveReload to true
+        liveReload: true,
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 });
