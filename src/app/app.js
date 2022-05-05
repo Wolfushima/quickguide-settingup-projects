f7ede00688602df4464b0fa46dc0b9ad00d02c2b
Adolfo Herrera <valherdolf@gmail.com>
2022-05-05 02:11:40 -0400

    Removed example1.js and its functionality

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,5 @@
 import collapseNavbar from './utils/collapse-navbar';
-import consoleWorld from './utils/example2';
 
 export default function run() {
     collapseNavbar();
-    consoleWorld();
 }
--- src/app/utils/example2.js
+++
@@ -1,3 +0,0 @@
-export default function consoleWorld() {
-  console.log('World');
-}
